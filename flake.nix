bdf717dc3b1fe0bdd93a8b9d3c182b4f3a22605b
Rasmus Buurman <rasmus@rbuurman.de>
2026-03-22 19:36:01 +0100

    Add nix flake stuff

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "DeDucktion - a proof editor and validator for Gentzen-style calculi";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "deducktion";
+          packages = with pkgs; [
+            nodejs
+            vscode-langservers-extracted
+            typescript-language-server
+          ];
+
+          shellHook = "unset SOURCE_DATE_EPOCH";
+        };
+      }
+    );
+}
