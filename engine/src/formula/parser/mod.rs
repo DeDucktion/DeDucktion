5eab350fe8d4051b0ce4126126feb440b0e353ad
Rasmus Buurman <rasmus@rbuurman.de>
2026-03-29 01:54:32 +0100

    ParsingSettings

--- engine/src/formula/parser/mod.rs
+++ engine/src/formula/parser/mod.rs
@@ -4,62 +4,80 @@ use chumsky::prelude::*;
 use crate::formula::{BinaryConnective, Formula, UnaryConnective};
 
 use lexer::Token;
+use settings::ParsingSettings;
 
 pub mod lexer;
 pub mod settings;
 
 /// Parses a formula that may be marked as discharged
-pub fn formula_parser<'tok, I>()
--> impl Parser<'tok, I, (Formula, bool), extra::Err<Rich<'tok, Token>>>
+pub fn formula_parser<'tok, I>(
+    settings: &ParsingSettings,
+) -> impl Parser<'tok, I, (Formula, bool), extra::Err<Rich<'tok, Token>>>
 where
     I: ValueInput<'tok, Token = Token, Span = SimpleSpan>,
 {
-    let not_discharged = formula().map(|f| (f, false));
+    let formula = match settings.parenthesization_style {
+        settings::ParenthesizationStyle::Strict => formula_strict().boxed(),
+        settings::ParenthesizationStyle::Lax => formula_lax().boxed(),
+    };
 
-    let discharged = formula()
+    let not_discharged = formula.clone().map(|f| (f, false));
+
+    let discharged = formula
         .delimited_by(just(Token::BracketOpen), just(Token::BracketClose))
         .map(|f| (f, true));
 
     discharged.or(not_discharged)
 }
 
-fn formula<'tok, I>() -> impl Parser<'tok, I, Formula, extra::Err<Rich<'tok, Token>>>
+// Fully parenthesized formula.
+fn formula_strict<'tok, I>() -> impl Parser<'tok, I, Formula, extra::Err<Rich<'tok, Token>>> + Clone
 where
     I: ValueInput<'tok, Token = Token, Span = SimpleSpan>,
 {
     recursive(|formula| {
-        let var = select! { Token::Ident(var) => Formula::Var(var) };
-
-        let atom = choice((
-            var,
-            formula
-                .clone()
-                .delimited_by(just(Token::ParenOpen), just(Token::ParenClose)),
-        ));
+        let var = select! { Token::Var(var) => Formula::Var(var) };
 
         let unary = unary_connective()
-            .repeated()
-            .foldr(atom.clone(), |connective, arg| Formula::Unary {
+            .then(formula.clone())
+            .map(|(connective, arg)| Formula::Unary {
                 connective,
                 arg: Box::new(arg),
             });
 
-        let atom2 = unary.clone().or(atom.clone());
-
-        let binary = atom2
+        let binary = formula
             .clone()
             .then(binary_connective())
-            .then(atom2.clone())
+            .then(formula.clone())
+            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
             .map(|((lhs, connective), rhs)| Formula::Binary {
                 connective,
                 lhs: Box::new(lhs),
                 rhs: Box::new(rhs),
             });
 
-        let atom3 = binary.or(atom2);
-
-        atom3
+        choice((var, unary, binary))
     })
+}
+
+// For now, the lax version only allows to omit outermost parentheses.
+fn formula_lax<'tok, I>() -> impl Parser<'tok, I, Formula, extra::Err<Rich<'tok, Token>>> + Clone
+where
+    I: ValueInput<'tok, Token = Token, Span = SimpleSpan>,
+{
+    let strict = formula_strict();
+
+    let top_level_binary = strict
+        .clone()
+        .then(binary_connective())
+        .then(strict.clone())
+        .map(|((lhs, connective), rhs)| Formula::Binary {
+            connective,
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        });
+
+    choice((top_level_binary, strict))
 }
 
 fn unary_connective<'tok, I>()
