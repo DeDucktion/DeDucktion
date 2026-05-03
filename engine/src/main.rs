5c6df273f0fb88969e203f7068e0357006f4aa81
Rasmus Buurman <rasmus@rbuurman.de>
2026-05-03 19:51:32 +0200

    example in main

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,8 +1,36 @@
+use std::collections::{HashMap, HashSet};
+
+use deducktion_engine::nd;
 use deducktion_engine::prop::formula::Formula;
 use deducktion_engine::prop::parser::settings::ParsingSettings;
 
 fn main() {
     let settings = ParsingSettings::default();
-    let formula = Formula::parse("not (not A and B)", &settings);
-    let _ = dbg!(formula);
+
+    let proof = nd::Derivation {
+        rule: Some(String::from("nd.and.intro")),
+        premises: vec![
+            nd::Derivation {
+                rule: None,
+                premises: vec![],
+                conclusion: nd::Judgement::parse("a", &settings).unwrap(),
+            },
+            nd::Derivation {
+                rule: None,
+                premises: vec![],
+                conclusion: nd::Judgement::parse("b", &settings).unwrap(),
+            },
+        ],
+        conclusion: nd::Judgement::parse("a and b", &settings).unwrap(),
+    };
+
+    let mut rules = HashMap::new();
+    rules.insert(String::from("nd.and.intro"), nd::rules::and_intro());
+    rules.insert(String::from("nd.or.elim"), nd::rules::or_elim());
+
+    let mut context = HashSet::new();
+    context.insert(Formula::parse("a", &settings).unwrap());
+    context.insert(Formula::parse("b", &settings).unwrap());
+
+    dbg!(proof.check(&rules, &context, &HashSet::new()));
 }
